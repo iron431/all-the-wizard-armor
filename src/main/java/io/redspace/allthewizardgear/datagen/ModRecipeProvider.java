cb7938fb76ba93cc738fb092b17c0a96502bfc5e
iron431 <34083081+iron431@users.noreply.github.com>
2023-10-24 10:41:02 -0500

    spell book recipes

--- src/main/java/io/redspace/allthewizardgear/datagen/ModRecipeProvider.java
+++ src/main/java/io/redspace/allthewizardgear/datagen/ModRecipeProvider.java
@@ -24,16 +24,19 @@ public class ModRecipeProvider extends RecipeProvider {
         smithing(consumer, ModRegistry.ATM_SMITHING.get(), ModTags.BASE_WIZARD_CHESTPLATE, TagRegistry.ALLTHEMODIUM_INGOT, ItemRegistry.ALLTHEMODIUM_MAGE_CHESTPLATE.get());
         smithing(consumer, ModRegistry.ATM_SMITHING.get(), ModTags.BASE_WIZARD_LEGGINGS, TagRegistry.ALLTHEMODIUM_INGOT, ItemRegistry.ALLTHEMODIUM_MAGE_LEGGINGS.get());
         smithing(consumer, ModRegistry.ATM_SMITHING.get(), ModTags.BASE_WIZARD_BOOTS, TagRegistry.ALLTHEMODIUM_INGOT, ItemRegistry.ALLTHEMODIUM_MAGE_BOOTS.get());
+        smithing(consumer, ModRegistry.ATM_SMITHING.get(), io.redspace.ironsspellbooks.registries.ItemRegistry.NETHERITE_SPELL_BOOK, TagRegistry.ALLTHEMODIUM_INGOT, ItemRegistry.ALLTHEMODIUM_SPELLBOOK.get());
 
         smithing(consumer, ModRegistry.VIB_SMITHING.get(), ItemRegistry.ALLTHEMODIUM_MAGE_HELMET, TagRegistry.VIBRANIUM_INGOT, ItemRegistry.VIBRANIUM_MAGE_HELMET.get());
         smithing(consumer, ModRegistry.VIB_SMITHING.get(), ItemRegistry.ALLTHEMODIUM_MAGE_CHESTPLATE, TagRegistry.VIBRANIUM_INGOT, ItemRegistry.VIBRANIUM_MAGE_CHESTPLATE.get());
         smithing(consumer, ModRegistry.VIB_SMITHING.get(), ItemRegistry.ALLTHEMODIUM_MAGE_LEGGINGS, TagRegistry.VIBRANIUM_INGOT, ItemRegistry.VIBRANIUM_MAGE_LEGGINGS.get());
         smithing(consumer, ModRegistry.VIB_SMITHING.get(), ItemRegistry.ALLTHEMODIUM_MAGE_BOOTS, TagRegistry.VIBRANIUM_INGOT, ItemRegistry.VIBRANIUM_MAGE_BOOTS.get());
+        smithing(consumer, ModRegistry.VIB_SMITHING.get(), ItemRegistry.ALLTHEMODIUM_SPELLBOOK, TagRegistry.VIBRANIUM_INGOT, ItemRegistry.VIBRANIUM_SPELLBOOK.get());
 
         smithing(consumer, ModRegistry.UNO_SMITHING.get(), ItemRegistry.VIBRANIUM_MAGE_HELMET, TagRegistry.UNOBTAINIUM_INGOT, ItemRegistry.UNOBTAINIUM_MAGE_HELMET.get());
         smithing(consumer, ModRegistry.UNO_SMITHING.get(), ItemRegistry.VIBRANIUM_MAGE_CHESTPLATE, TagRegistry.UNOBTAINIUM_INGOT, ItemRegistry.UNOBTAINIUM_MAGE_CHESTPLATE.get());
         smithing(consumer, ModRegistry.UNO_SMITHING.get(), ItemRegistry.VIBRANIUM_MAGE_LEGGINGS, TagRegistry.UNOBTAINIUM_INGOT, ItemRegistry.UNOBTAINIUM_MAGE_LEGGINGS.get());
         smithing(consumer, ModRegistry.UNO_SMITHING.get(), ItemRegistry.VIBRANIUM_MAGE_BOOTS, TagRegistry.UNOBTAINIUM_INGOT, ItemRegistry.UNOBTAINIUM_MAGE_BOOTS.get());
+        smithing(consumer, ModRegistry.UNO_SMITHING.get(), ItemRegistry.VIBRANIUM_SPELLBOOK, TagRegistry.UNOBTAINIUM_INGOT, ItemRegistry.UNOBTAINIUM_SPELLBOOK.get());
     }
 
     protected static void smithing(Consumer<FinishedRecipe> consumer, Item smithingTemplate, TagKey<Item> baseItem, TagKey<Item> modifierItem, Item result) {
