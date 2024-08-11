3394ee193e8b71f340259a75b78c9dc8a5288794
iron431 <34083081+iron431@users.noreply.github.com>
2024-08-11 15:06:04 -0500

    finish port

--- src/main/java/io/redspace/allthewizardgear/registry/CreativeTabRegistry.java
+++ src/main/java/io/redspace/allthewizardgear/registry/CreativeTabRegistry.java
@@ -14,7 +14,6 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
 
-@EventBusSubscriber
 public class CreativeTabRegistry {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AllTheWizardGear.MODID);
 
@@ -23,28 +22,10 @@ public class CreativeTabRegistry {
     }
 
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MOD_ARMOR_TAB = CREATIVE_MODE_TABS.register("allthewizardarmor_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
             .title(Component.translatable("tab.allthewizardarmor.armor"))
             .icon(() -> ItemRegistry.UNOBTAINIUM_MAGE_CHESTPLATE.get().getDefaultInstance())
-            .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
-            .displayItems((parameters, output) -> {
-                output.accept(ItemRegistry.ALLTHEMODIUM_MAGE_HELMET.get());
-                output.accept(ItemRegistry.ALLTHEMODIUM_MAGE_CHESTPLATE.get());
-                output.accept(ItemRegistry.ALLTHEMODIUM_MAGE_LEGGINGS.get());
-                output.accept(ItemRegistry.ALLTHEMODIUM_MAGE_BOOTS.get());
+            .withTabsBefore(CreativeModeTabs.SPAWN_EGGS).build());
 
-                output.accept(ItemRegistry.VIBRANIUM_MAGE_HELMET.get());
-                output.accept(ItemRegistry.VIBRANIUM_MAGE_CHESTPLATE.get());
-                output.accept(ItemRegistry.VIBRANIUM_MAGE_LEGGINGS.get());
-                output.accept(ItemRegistry.VIBRANIUM_MAGE_BOOTS.get());
-
-                output.accept(ItemRegistry.UNOBTAINIUM_MAGE_HELMET.get());
-                output.accept(ItemRegistry.UNOBTAINIUM_MAGE_CHESTPLATE.get());
-                output.accept(ItemRegistry.UNOBTAINIUM_MAGE_LEGGINGS.get());
-                output.accept(ItemRegistry.UNOBTAINIUM_MAGE_BOOTS.get());
-            }).build());
-
-    @SubscribeEvent
     public static void fillCreativeTab(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == MOD_ARMOR_TAB.getKey()) {
             ItemRegistry.items().stream().map(DeferredHolder::get).forEach(event::accept);
